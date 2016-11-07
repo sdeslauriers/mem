34353491b82dd8ade972cf962e14628db3137a23
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2016-11-07 12:42:39 -0500

    Add a class for evidence tables
    Evidence tables are probability tables that can be updated by evidence.
    They can depend only on a single variable.
    
    To prevent code repetition, a new BaseTable class was added. This new
    class implements the functionality which is common to both the
    ConditionalProbability and Evidence classes of the mem.network.tables
    module.

--- mem/network/tables.py
+++ mem/network/tables.py
@@ -5,6 +5,53 @@ import itertools
 import operator
 
 
+class BaseTable(object):
+    def __init__(self, variables):
+        """Base class for tables"""
+
+        # Support both a single variable and an iterable of variables.
+        if isinstance(variables, collections.abc.Iterable):
+            variables = list(variables)
+        else:
+            variables = [variables]
+        self._variables = variables
+
+    def __str__(self):
+        """String representation of the conditional probability table"""
+
+        out = ''
+        states = [range(v.nb_states) for v in self.variables]
+        for state, p in zip(itertools.product(*states), self.probabilities):
+            out += ' '.join((str(s) for s in state))
+            out += ' | {}\n'.format(p)
+
+        return out
+
+    @property
+    def nb_states(self):
+        return reduce(operator.mul, [v.nb_states for v in self.variables])
+
+    @property
+    def probabilities(self):
+        raise NotImplementedError(
+            'The BaseTable class does not implement the probabilities '
+            'property. It must be implemented by a subclass.')
+
+    @property
+    def variables(self):
+        return self._variables
+
+    def validate_probabilities(self, probabilities):
+
+        # The number of probabilities must match the number of possible
+        # states.
+        if len(probabilities) != self.nb_states:
+            raise ValueError(
+                'The number of probabilities does not match the number of '
+                'possible states ({} != {}).'
+                .format(len(probabilities), self.nb_states))
+
+
 class ConditionalProbability(object):
     def __init__(self, variables, probabilities):
         """Conditional probability table"""
@@ -49,3 +96,26 @@ class ConditionalProbability(object):
     @property
     def variables(self):
         return self._variables
+
+
+class Evidence(BaseTable):
+    def __init__(self, variable, probabilities):
+        """Evidence probability table"""
+
+        # Evidence tables can only depend on a single variable.
+        if isinstance(variable, collections.abc.Iterable):
+            if len(variable) != 1:
+                raise ValueError(
+                    'Evidence tables can only depend on a single variable.')
+
+        super().__init__(variable)
+        self.validate_probabilities(probabilities)
+        self._probabilities = probabilities
+
+    @property
+    def probabilities(self):
+        return self._probabilities
+
+    def update(self, probabilities):
+        self.validate_probabilities(probabilities)
+        self._probabilities = probabilities
--- tests/network/test_tables.py
+++ tests/network/test_tables.py
@@ -36,3 +36,56 @@ class Table(unittest.TestCase):
             ValueError,
             mem.network.tables.ConditionalProbability,
             cluster, [0.5, 0.4, 0.05, 0.05])
+
+
+class Evidence(unittest.TestCase):
+
+    def test_init(self):
+
+        # Test normal behavior.
+        off_mean = np.matrix(np.zeros((2, 1)))
+        on_mean = np.matrix(np.ones((2, 1)))
+        variance = np.matrix(np.identity(2))
+        on_state = mem.network.variables.GaussianState(on_mean, variance)
+        off_state = mem.network.variables.GaussianState(off_mean, variance)
+        cluster = mem.network.variables.Cluster([0, 1], [off_state, on_state])
+        table = mem.network.tables.Evidence(cluster, [0.5, 0.5])
+
+        self.assertEqual(table.variables[0], cluster)
+        self.assertEqual(table.nb_states, 2)
+        np.testing.assert_array_equal(table.probabilities, [0.5, 0.5])
+
+        # The number of probabilities must match the number of states.
+        self.assertRaises(
+            ValueError,
+            mem.network.tables.Evidence,
+            cluster, [0.5, 0.3, 0.1, 0.1])
+
+        # Evidence tables can only depend on a single variable.
+        other_cluster = mem.network.variables.Cluster(
+            [0, 1], [off_state, on_state])
+        self.assertRaises(
+            ValueError,
+            mem.network.tables.Evidence,
+            [cluster, other_cluster], [0.5, 0.2, 0.1, 0.2])
+
+    def test_update(self):
+
+        off_mean = np.matrix(np.zeros((2, 1)))
+        on_mean = np.matrix(np.ones((2, 1)))
+        variance = np.matrix(np.identity(2))
+        on_state = mem.network.variables.GaussianState(on_mean, variance)
+        off_state = mem.network.variables.GaussianState(off_mean, variance)
+        cluster = mem.network.variables.Cluster([0, 1], [off_state, on_state])
+        table = mem.network.tables.Evidence(cluster, [0.5, 0.5])
+
+        # The probabilities of an evidence table can be updated.
+        table.update([0.2, 0.8])
+        np.testing.assert_array_equal(table.probabilities, [0.2, 0.8])
+
+        # The number of probabilities must match the number of possible
+        # states.
+        self.assertRaises(
+            ValueError,
+            table.update,
+            [1.0])
