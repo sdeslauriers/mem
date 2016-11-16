0508d21fde226d21f56f83536fb7fa757825b991
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2016-11-16 10:42:05 -0500

    Simplify ConditionalProbability by using BaseTable
    The mem.network.tables.ConditionalProbability was simplified by making
    it a subclass of BaseTable. The idea is that a base table represents a
    list of probabilities associated with a list of variable states. On the
    other hand, a conditional probability table allows more manipulation,
    such as marginalization and table products.

--- mem/network/tables.py
+++ mem/network/tables.py
@@ -52,50 +52,21 @@ class BaseTable(object):
                 .format(len(probabilities), self.nb_states))
 
 
-class ConditionalProbability(object):
+class ConditionalProbability(BaseTable):
     def __init__(self, variables, probabilities):
         """Conditional probability table"""
 
-        # Support both a single variable and an iterable of variables.
-        if isinstance(variables, collections.abc.Iterable):
-            variables = list(variables)
-        else:
-            variables = [variables]
-
-        # The number of probabilities must match the number of possible
-        # states.
-        nb_states = reduce(operator.mul, [v.nb_states for v in variables])
-        if len(probabilities) != nb_states:
-            raise ValueError(
-                'The number of probabilities does not match the number of '
-                'possible states ({} != {}).'
-                .format(len(probabilities), nb_states))
-
-        self._variables = variables
+        super().__init__(variables)
+        self.validate_probabilities(probabilities)
         self._probabilities = probabilities
-
-    def __str__(self):
-        """String representation of the conditional probability table"""
-
-        out = ''
-        states = [range(v.nb_states) for v in self.variables]
-        for state, p in zip(itertools.product(*states), self.probabilities):
-            out += ' '.join((str(s) for s in state))
-            out += ' | {}\n'.format(p)
-
-        return out
-
-    @property
-    def nb_states(self):
-        return len(self.probabilities)
 
     @property
     def probabilities(self):
         return self._probabilities
 
-    @property
-    def variables(self):
-        return self._variables
+    def update(self, probabilities):
+        self.validate_probabilities(probabilities)
+        self._probabilities = probabilities
 
 
 class Evidence(BaseTable):
