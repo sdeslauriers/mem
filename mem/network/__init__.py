7dcc335b7c23e18cc64567f22ce1d0a5cea0daa0
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2016-11-17 16:41:51 -0500

    Add log_partition method to the network
    The log partition of a network is one term in the entropy of a MEM
    solution. It represents the likelyhood of an arrangement of sources.
    
    The log_partition method of the network class evaluates the log
    partition at a specific value of the Lagrange multipliers. The current
    implementation is naive and will need to be improved.

--- mem/network/__init__.py
+++ mem/network/__init__.py
@@ -1,4 +1,10 @@
 
+from functools import reduce
+
+import numpy as np
+
+import mem.network.tables
+
 
 class Bayesian(object):
     def __init__(self):
@@ -6,6 +12,7 @@ class Bayesian(object):
 
         self._variables = []
         self._tables = []
+        self._interface = []
 
     def add_variable(self, variable):
 
@@ -37,3 +44,53 @@ class Bayesian(object):
             past_index = index
 
         self._tables.append(table)
+
+    def marginalize(self, variable):
+        """Marginalizes a variable out of the Bayesian network
+
+        Removes a variable from the domain of the Bayesian network by summing
+        it out. The result is a new Bayesian network.
+
+        Args:
+            variable (bayesian.Variable) : The variable to be removed.
+
+        """
+
+        # Find all tables with the variable in their domain.
+        tables = []
+        for table in self._interface:
+            if variable in table.domain:
+                tables.append(table)
+
+        # Compute the product of all tables and marginalize the variable out
+        # of the result.
+        def mul(left, right):
+            return mem.network.tables.Product(self._variables, left, right)
+        new_table = reduce(mul, tables)
+        new_table = new_table.marginalize(variable)
+
+        # Remove all table with the variable in their domain and add the
+        # new one.
+        for table in tables:
+            self._interface.remove(table)
+        self._interface.append(new_table)
+
+    def log_partition(self, lagrange):
+        """Evaluate the log partition of the network"""
+
+        # Update all evidence tables.
+        for table in self._tables:
+            if isinstance(table, mem.network.tables.Evidence):
+                table.update(lagrange)
+
+        # Marginalize all variables except the first one.
+        self._interface = self._tables
+        for variable in self._variables[1:]:
+            self.marginalize(variable)
+
+        # Compute the product of all remaining tables.
+        def mul(left, right):
+            return mem.network.tables.Product(self._variables, left, right)
+        new_table = reduce(mul, self._interface)
+
+        return np.log(np.sum(new_table.probabilities))
--- tests/network/test_init.py
+++ tests/network/test_init.py
@@ -43,3 +43,31 @@ class Bayesian(unittest.TestCase):
         network = mem.network.Bayesian()
         network.add_variable(cluster)
         self.assertRaises(ValueError, network.add_variable, cluster)
+
+    def test_log_partition(self):
+
+        # Test normal behavior using a simple network with one evidence
+        # table.
+        cluster = mem.network.variables.ZeroOneCluster(range(2))
+        table = mem.network.tables.Evidence(cluster, [0.5, 0.5])
+        network = mem.network.Bayesian()
+        network.add_variable(cluster)
+        network.add_table(table)
+        entropy = network.log_partition(np.matrix(np.zeros((2, 1))))
+        self.assertAlmostEqual(entropy, np.log(2))
+
+        # Test normal behavior using a network with one evidence table and
+        # a prior.
+        cluster = mem.network.variables.ZeroOneCluster(range(3))
+        evidence = mem.network.tables.Evidence(cluster, [0.5, 0.5])
+        prior = mem.network.tables.ConditionalProbability(cluster, [0.9, 0.1])
+        network = mem.network.Bayesian()
+        network.add_variable(cluster)
+        network.add_table(evidence)
+        network.add_table(prior)
+        entropy = network.log_partition(np.matrix(np.zeros((3, 1))))
+        self.assertAlmostEqual(entropy, 0.0)
+        entropy = network.log_partition(np.matrix(np.ones((3, 1))))
+        self.assertAlmostEqual(entropy, 1.067656, 5)
+        entropy = network.log_partition(np.matrix([[-1], [1], [-1]]))
+        self.assertAlmostEqual(entropy, -0.065298, 5)
